78c55999c0fa673e8d69eca87d2da7065e5fcf40
Christian Ravnikar <christian.ravnikar@gmail.com>
2021-07-06 22:06:45 +0200

    Dodan piškotek

--- vislice.py
+++ vislice.py
@@ -7,22 +7,41 @@ vislice = model.Vislice()
 def index():
     return bottle.template("index.tpl")
 
-@bottle.post("/igra/")
+# @bottle.post("/igra/")
+# def nova_igra():
+#     id_igre = vislice.nova_igra()
+#     novi_url = f"/igra/{id_igre}/"
+#     bottle.redirect(novi_url)
+
+@bottle.post("/nova-igra/")
 def nova_igra():
     id_igre = vislice.nova_igra()
-    novi_url = f"/igra/{id_igre}/"
-    bottle.redirect(novi_url)
+    bottle.response.set_cookie('idigre', f'idigre{id_igre}', path='/')
+    bottle.redirect('/igra/')
 
-@bottle.get("/igra/<id_igre:int>/")
-def pokazi_igro(id_igre):
+# @bottle.get("/igra/<id_igre:int>/")
+# def pokazi_igro(id_igre):
+#     trenutna_igra, trenutno_stanje = vislice.igre[id_igre]
+#     return bottle.template("igra.tpl", igra=trenutna_igra, stanje=trenutno_stanje)
+
+@bottle.get('/igra/')
+def pokazi_igro():
+    id_igre = int(bottle.request.get_cookie('idigre').split('e')[1])
     trenutna_igra, trenutno_stanje = vislice.igre[id_igre]
-    return bottle.template("igra.tpl", igra=trenutna_igra, stanje=trenutno_stanje)
+    return bottle.template('igra.tpl', igra=trenutna_igra, stanje=trenutno_stanje)
 
-@bottle.post("/igra/<id_igre:int>/")
-def ugibaj_na_igri(id_igre):
+# @bottle.post("/igra/<id_igre:int>/")
+# def ugibaj_na_igri(id_igre):
+#     ugibana = bottle.request.forms["crka"]
+#     vislice.ugibaj(id_igre, ugibana)
+#     return bottle.redirect(f"/igra/{id_igre}/")
+
+@bottle.post('/igra/')
+def ugibaj_na_igri():
+    id_igre = int(bottle.request.get_cookie('idigre').split('e')[1])
     ugibana = bottle.request.forms["crka"]
     vislice.ugibaj(id_igre, ugibana)
-    return bottle.redirect(f"/igra/{id_igre}/")
+    return bottle.redirect('/igra/')
 
 
 @bottle.route("/img/<file_path:path>")
